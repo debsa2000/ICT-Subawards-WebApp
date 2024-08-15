1acadac84513173f12796a2153b9612d853890c4
debsa2000 <debsa2000@gmail.com>
2024-08-15 12:10:13 -0500

    removed navbar style due to error

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ from streamlit_navigation_bar import st_navbar
 
 page_names=['Project View','Analytics']
 logopath="images/ICTlogo.svg"
-styles = {
-    "nav": {
-        "background-color": " #FF5F05",
-        "justify-content": "left",
-    }
-}
-nav_bar = st_navbar(pages=page_names, logo_path=logopath, styles=styles)
+# styles = {
+#     "nav": {
+#         "background-color": " #FF5F05",
+#         "justify-content": "left",
+#     }
+# }
+nav_bar = st_navbar(pages=page_names, logo_path=logopath)
 
 df = get_dataframe()
 
