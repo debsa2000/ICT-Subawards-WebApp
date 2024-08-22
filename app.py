1734672da959c8106a605eb97e22c916002580c8
debsa2000 <debsa2000@gmail.com>
2024-08-22 10:30:08 -0500

    updated pie chart - not required by year

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import pandas as pd
 import streamlit as st
-import plotly.graph_objects as go
 import plotly.express as px
 from preprocessing import get_dataframe
 from streamlit_navigation_bar import st_navbar
+
+print("Starting app.py")
 
 page_names=['Project View','Analytics']
 logopath="images/ICTlogo.svg"
@@ -16,79 +16,44 @@ logopath="images/ICTlogo.svg"
 nav_bar = st_navbar(pages=page_names, logo_path=logopath)
 
 df = get_dataframe()
+df.rename(columns={ df.columns[6]: "FY18-FY24 Subaward Total" }, inplace = True)
+st.dataframe(df.astype(str))
 
 all_projects_list = df['Project Number:'].unique().tolist()
 project_selected = st.selectbox('Project Number:', all_projects_list)
 
-df_one = pd.DataFrame()
 df_one = df[df['Project Number:']==project_selected]
 df_one = df_one.transpose()
 df_one = df_one.reset_index()
-# df_one.dropna(inplace=True)
 df_one.columns = ['Project Attribute', 'Value']
 df_one = df_one[df_one['Value'].notna()]
 
 st.dataframe(df_one.astype(str))
 
-years_selected = st.multiselect("Select years to show pie chart",["2018", "2019", "2020", "2021", "2022", "2023", "2024"], default=["2018", "2019", "2020", "2021", "2022", "2023", "2024"])
+sum_IDOTShare = 0
+sum_CostShare = 0
+sum_IDOTSubAdminCost = 0
+for i, row in df_one.iterrows():
+    if "IDOT Share FY" in row['Project Attribute']:
+        sum_IDOTShare = sum_IDOTShare+row['Value']
+        print("sum_IDOTShare:", sum_IDOTShare)
+    if "Cost Share FY" in row['Project Attribute']:
+        sum_CostShare = sum_CostShare+row['Value']
+        print("sum_CostShare:", sum_CostShare)
+    if "IDOT Sub Admin Cost FY" in row['Project Attribute']:
+        sum_IDOTSubAdminCost = sum_IDOTSubAdminCost+row['Value']
+        print("sum_IDOTSubAdminCost:", sum_IDOTSubAdminCost)
 
-def check_empty_plot(figure):
-    def check_plot(plot):
-        # for plots like area, bar, violin and timeline
-        if "x" in plot and plot["x"] is None:
-            return True
-        # for plots like choropleth
-        elif "z" in plot and len(plot["z"]) == 0:
-            return True
-        # for plots like sunburst and treemap
-        elif "values" in plot and plot["values"] is None:
-            return True
-        # for plots like line_mapbox
-        elif "lat" in plot and len(plot["lat"]) == 0:
-            return True
-        return False
+total_sum=sum_IDOTShare+sum_CostShare+sum_IDOTSubAdminCost
+total_alocated_budget = df_one.loc[6, 'Value']
 
-    # check if it is an empty go.Figure
-    if figure.data == tuple():
-        return True
-    return all(check_plot(plot) for plot in figure.data)
+pie_chart = px.pie(values=[sum_IDOTShare, sum_CostShare, sum_IDOTSubAdminCost], names=['sum_IDOTShare', 'sum_CostShare', 'sum_IDOTSubAdminCost'])
+st.plotly_chart(pie_chart)
 
-for e in years_selected:
-    if e=="2018":
-        df_one_year = df_one[df_one['Project Attribute'].isin(["IDOT Share FY2018", "Cost Share FY2018", "IDOT Sub Admin Cost FY2018"])]
-        pie_chart = px.pie(df_one_year, values='Value', names='Project Attribute')
-        if check_empty_plot(pie_chart)==False:
-            st.plotly_chart(pie_chart)
-    if e=="2019":
-        df_one_year = df_one[df_one['Project Attribute'].isin(["IDOT Share FY2019", "Cost Share FY2019", "IDOT Sub Admin Cost FY2019"])]
-        pie_chart = px.pie(df_one_year, values='Value', names='Project Attribute')
-        if check_empty_plot(pie_chart)==False:
-            st.plotly_chart(pie_chart)
-    if e=="2020":
-        df_one_year = df_one[df_one['Project Attribute'].isin(["IDOT Share FY2020", "Cost Share FY2020", "IDOT Sub Admin Cost FY2020"])]
-        pie_chart = px.pie(df_one_year, values='Value', names='Project Attribute')
-        if check_empty_plot(pie_chart)==True:
-            st.plotly_chart(pie_chart)
-    if e=="2021":
-        df_one_year = df_one[df_one['Project Attribute'].isin(["IDOT Share FY2021", "Cost Share FY2021", "IDOT Sub Admin Cost FY2021"])]
-        pie_chart = px.pie(df_one_year, values='Value', names='Project Attribute')
-        if check_empty_plot(pie_chart)==True:
-            st.plotly_chart(pie_chart)
-    if e=="2022":
-        df_one_year = df_one[df_one['Project Attribute'].isin(["IDOT Share FY2022", "Cost Share FY2022", "IDOT Sub Admin Cost FY2022"])]
-        pie_chart = px.pie(df_one_year, values='Value', names='Project Attribute')
-        if check_empty_plot(pie_chart)==True:
-            st.plotly_chart(pie_chart)
-    if e=="2023":
-        df_one_year = df_one[df_one['Project Attribute'].isin(["IDOT Share FY2023", "Cost Share FY2023", "IDOT Sub Admin Cost FY2023"])]
-        pie_chart = px.pie(df_one_year, values='Value', names='Project Attribute')
-        if check_empty_plot(pie_chart)==True:
-            st.plotly_chart(pie_chart)
-    if e=="2024":
-        df_one_year = df_one[df_one['Project Attribute'].isin(["IDOT Share FY2024", "Cost Share FY2024", "IDOT Sub Admin Cost FY2024"])]
-        pie_chart = px.pie(df_one_year, values='Value', names='Project Attribute')
-        if check_empty_plot(pie_chart)==True:
-            st.plotly_chart(pie_chart)
+if total_sum==total_alocated_budget:
+    st.text("FY18-24 Subaward Total is equal to IDOT share + Cost share + Subadmin cost")
+else:
+    st.text("FY18-24 Subaward Total is not equal to IDOT share + Cost share + Subadmin cost")
 
 
 with st.form("add_new_invoice_form"):
@@ -96,13 +61,13 @@ with st.form("add_new_invoice_form"):
     
     inv_start_date = st.date_input("Select start date for invoice billing period:", format="MM-DD-YYYY", value=None)
     inv_end_date = st.date_input("Select end date for invoice billing period:", format="MM-DD-YYYY", value=None)
-    print(inv_start_date, inv_start_date)
+    # print(inv_start_date, inv_start_date)
     
     invoice_amount = st.text_input("Enter invoice amount:")
-    print(invoice_amount)
+    # print(invoice_amount)
     
     invoice_cost_share = st.text_input("Enter invoice cost share amount:")
-    print(invoice_cost_share)
+    # print(invoice_cost_share)
    
     submitted = st.form_submit_button("Submit")
     
