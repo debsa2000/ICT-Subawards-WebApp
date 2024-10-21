7b892cbf9187ea08ff14358605da52ef700f6479
debsa2000 <debsa2000@gmail.com>
2024-10-21 16:32:23 -0500

    double check changes are reflected on server

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ style = {
         "justify-content": "left",
     }
 }
-page = st_navbar(pages=["All Projects View", "Project View", "Add Invoice", "Project", "IGA"], logo_path='images/ICTlogo.svg',styles=style)
+page = st_navbar(pages=["All Projects View", "Project View", "Invoice", "Project", "IGA"], logo_path='images/ICTlogo.svg',styles=style)
 
 if page == "All Projects View":
     app_pages.all_projects_view.all_projects_view_func()
 elif page == "Project View":
     app_pages.project_view.project_view_func()
-elif page == "Add Invoice":
+elif page == "Invoice":
     app_pages.add_invoice.add_invoice_func()
 elif page == "Project":
     app_pages.add_project.add_project_func()
