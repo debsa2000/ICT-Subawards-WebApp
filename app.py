e1a467f4e217e60a7d42e8c9e668b2f44b539d07
debsa2000 <debsa2000@gmail.com>
2024-10-21 16:13:47 -0500

    check automatic workflow run

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ style = {
         "justify-content": "left",
     }
 }
-page = st_navbar(pages=["All Projects View", "Project View", "Add Invoice", "Add Project", "IGA"], logo_path='images/ICTlogo.svg',styles=style)
+page = st_navbar(pages=["All Projects View", "Project View", "Add Invoice", "Project", "IGA"], logo_path='images/ICTlogo.svg',styles=style)
 
 if page == "All Projects View":
     app_pages.all_projects_view.all_projects_view_func()
@@ -15,7 +15,7 @@ elif page == "Project View":
     app_pages.project_view.project_view_func()
 elif page == "Add Invoice":
     app_pages.add_invoice.add_invoice_func()
-elif page == "Add Project":
+elif page == "Project":
     app_pages.add_project.add_project_func()
 elif page == "IGA":
     app_pages.add_iga.add_iga_func()
